9322475651fd1d2b8f9952eca3866c693660e63b
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2024-06-10 00:00:46 +0200

    add icon expand

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,24 @@ module.exports = {
     },
   },
   plugins: [
+  function({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = '') {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey]
+          let newVars
+          if (typeof value === 'string') {
+            newVars = { [`--color${colorGroup}-${colorKey}`.replace('-DEFAULT', '')]: value }
+          }
+          else {
+            newVars = extractColorVars(value, `-${colorKey}`)
+          }
+          return { ...vars, ...newVars }
+        }, {})
+      }
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      });
+    },
     plugin(function ({ addVariant }) {
       addVariant('request-opened', ['.request-opened &', '.request-opened&'])
       addVariant('formatted', ['.formatted &', '.formatted&'])
